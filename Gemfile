062d5c631537424c66862c624f377221b4eed6c6
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-23 10:31:31 +0000

    Feature test for first user story

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source'https://rubygems.org'
+
+ruby '2.4.1'
+
+gem 'capybara'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'rspec'
---
+++ spec/viewing_links_spec.rb
@@ -0,0 +1,15 @@
+feature 'Opening homepage' do
+  scenario 'homepage displays welcome text' do
+    visit('/')
+    expect(page).to have_content('Welcome to Bookmark Manager!')
+  end
+end
+
+
+feature 'viewing bookmarked links' do
+  scenario 'user can see the bookmarks' do
+    Link.create(url: 'http://ww.makersacademy.com', title: 'Makers Academy')
+    visit('/links')
+    expect(page).to have_content('Makers Academy')
+  end
+end
